f151c5f914f64728402ef75322a01a24a1b4952a
xiaoguang <zhxiaog@outlook.com>
2026-05-28 19:53:29 -0700

    feat: RuntimeListenerServer in executor

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -3,8 +3,10 @@ mod error;
 mod executor;
 mod provider;
 mod registry;
+mod runtime_listener;
 
 pub use error::{ExecutorError, RuntimeError};
 pub use executor::Executor;
 pub use provider::{HealthStatus, RuntimeHandle, RuntimeProvider};
 pub(crate) use connected_registry::{ConnectedRuntimeRegistry, RuntimeSink};
+pub(crate) use runtime_listener::RuntimeListenerServer;
