7375deb71f832d39579837e47b3ead1255c3c2ee
xiaoguang <zhxiaog@outlook.com>
2026-05-28 20:13:40 -0700

    feat: ExecutorClient + WsExecutorTransport

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,8 +1,10 @@
 mod error;
+mod executor_client;
 mod handler;
 mod registry;
 mod server;
 
 pub use error::ServerError;
+pub use executor_client::{ClientError, ExecutorClient, ExecutorTransport, WsExecutorTransport};
 pub use handler::ExecutorEventHandler;
 pub use server::Server;
