a798a6c07d4fea721b65dbeba841987e1d462d3b
xiaoguang <zhxiaog@outlook.com>
2026-05-28 19:54:01 -0700

    feat: ProcessRuntimeProvider

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -1,12 +1,14 @@
 mod connected_registry;
 mod error;
 mod executor;
+mod process_provider;
 mod provider;
 mod registry;
 mod runtime_listener;
 
 pub use error::{ExecutorError, RuntimeError};
 pub use executor::Executor;
+pub use process_provider::ProcessRuntimeProvider;
 pub use provider::{HealthStatus, RuntimeHandle, RuntimeProvider};
 pub(crate) use connected_registry::{ConnectedRuntimeRegistry, RuntimeSink};
 pub(crate) use runtime_listener::RuntimeListenerServer;
